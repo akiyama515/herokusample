8292293d49f947f82ef8aecb1da6c37612279b45
akiyama515 <akiyama@sisinc.co.jp>
2015-09-04 22:21:27 +0900

    test

--- twilio_response.php
+++ twilio_response.php
@@ -1,5 +1,10 @@
 <?php
 
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response>';
+echo    '<Say voice="woman" language="ja-jp">ファイルの最初です</Say>';
+echo '</Response>';
+
 require_once ("soapclient/SforcePartnerClient.php");
 
 // 事前に必要な情報を宣言します。   
