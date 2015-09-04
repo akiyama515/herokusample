b4d722e297000b8329f6a4d2ab4ae469a1b29c87
akiyama515 <akiyama@sisinc.co.jp>
2015-09-04 22:34:39 +0900

    test

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php echo "hello world!";?>
+<?php
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response>';
+echo    '<Say voice="woman" language="ja-jp">ファイルの最初です</Say>';
+echo '</Response>';
+
+?>
