f4da645ed0c96328b5102e7bd91956d4675fbf93
akiyama515 <akiyama@sisinc.co.jp>
2015-09-17 20:34:30 +0900

    update

--- twilioQuestion2.php
+++ twilioQuestion2.php
@@ -6,7 +6,7 @@
 ?>
 
 <Response>
-  <Gather timeout="60" numDigits="1" action="twilio_response.php>
+  <Gather timeout="60" numDigits="1" action="twilio_response.php">
     <Say voice="woman" language="ja-jp">
       質問2   興味を持った部署がかんソ部の人は1を。。産ソ部の人は2を。。金ソ部の人は3を。。ソ部の人は4を。押してください
     </Say>
