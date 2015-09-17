fbf799d0fd8b6d22722cabad59943db3a883b58d
akiyama515 <akiyama@sisinc.co.jp>
2015-09-17 21:56:18 +0900

    update

--- twilioQuestion2.php
+++ twilioQuestion2.php
@@ -10,7 +10,7 @@
   header("content-type: text/xml");
 ?>
 <Response>
-  <Gather timeout="60" numDigits="1" action="twilio_response.php?answer1=<?php  $answer1  ?>" method="GET">
+  <Gather timeout="60" numDigits="1" action="twilio_response.php?answer1=<?php  echo $answer1  ?>" method="GET">
     <Say voice="woman" language="ja-jp">
       質問2   興味を持った部署がかんソ部の人は1を。。産ソ部の人は2を。。金ソ部の人は3を。。ソ部の人は4を。押してください
     </Say>
